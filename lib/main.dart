cec8680b616559b6806e94b274c369e1c6b2131d
Diego Sandoval <16181660+sandov@users.noreply.github.com>
2024-01-31 18:08:17 -0300

    add google fonts, open sans as font, add container to text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -7,13 +8,21 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  static const String sample_text =
+      "When Zarathustra was thirty years old, he left his home and the lake of his home, and went into the mountains.";
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!', style: const TextStyle(fontSize: 32)),
-        ),
+            child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Text(sample_text,
+              style: GoogleFonts.openSans(
+                fontSize: 24,
+              )),
+        )),
       ),
     );
   }
