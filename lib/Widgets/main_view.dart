f67df9d952b30564c2cf640c2a19de7e9030fb99
Diego Sandoval <16181660+sandov@users.noreply.github.com>
2024-05-08 17:10:08 -0400

    Include epubx code

--- lib/Widgets/main_view.dart
+++ lib/Widgets/main_view.dart
@@ -1,29 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:nuez/Widgets/book_page.dart';
+import 'package:epubx/epubx.dart';
+import 'dart:io' as io;
+import 'package:path/path.dart' as path;
+import 'dart:async';
 
-class MainView extends StatelessWidget {
-  static const String sampleText =
-      """When Zarathustra was thirty years old, he left his home and the lake of his home, and went into the mountains. There he enjoyed his spirit and solitude, and for ten years did not weary of it. But at last his heart changed﻿—and rising one morning with the rosy dawn, he went before the sun, and spake thus unto it:
-Thou great star! What would be thy happiness if thou hadst not those for whom thou shinest!
-For ten years hast thou climbed hither unto my cave: thou wouldst have wearied of thy light and of the journey, had it not been for me, mine eagle, and my serpent.
-But we awaited thee every morning, took from thee thine overflow and blessed thee for it.
-Lo! I am weary of my wisdom, like the bee that hath gathered too much honey; I need hands outstretched to take it.
-I would fain bestow and distribute, until the wise have once more become joyous in their folly, and the poor happy in their riches.
-Therefore must I descend into the deep: as thou doest in the evening, when thou goest behind the sea, and givest light also to the netherworld, thou exuberant star!
-Like thee must I go down, as men say, to whom I shall descend.
-Bless me, then, thou tranquil eye, that canst behold even the greatest happiness without envy!
-Bless the cup that is about to overflow, that the water may flow golden out of it, and carry everywhere the reflection of thy bliss!
-Lo! This cup is again going to empty itself, and Zarathustra is again going to be a man.
-Thus began Zarathustra’s down-going.""";
-
+class MainView extends StatefulWidget {
   const MainView({super.key});
 
   @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  late Future<String> bookData;
+
+  @override
+  void initState() {
+    super.initState();
+    // Call getBook method inside initState
+    bookData = getBook();
+  }
+
+  Future<String> getBook() async {
+    String fileName = 'comedy.epub';
+    String fullPath = path.join(io.Directory.current.path, fileName);
+    var targetFile = io.File(fullPath);
+    List<int> bytes = await targetFile.readAsBytes();
+
+    EpubBook epubBook = await EpubReader.readBook(bytes);
+
+    String title = epubBook.Title ?? "Empty title";
+    return title;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
+    return SingleChildScrollView(
       child: Center(
         child: Column(
-          children: <BookPage>[BookPage(sampleText), BookPage(sampleText)],
+          children: [
+            FutureBuilder<String>(
+              future: bookData, // Use the stored future here
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  // Use snapshot.data to access the fetched data
+                  return BookPage('${snapshot.data}');
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
